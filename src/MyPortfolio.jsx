82242f74bb717d7359f46b10a8fcb9aa89a4fff2
klylylydeee <klylylydeee@gmail.com>
2021-01-14 13:19:05 +0800

    Replace animation script with dependency

---
+++ src/MyPortfolio.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './components/styles/class.scss';
+import './myPortfolio.scss'
+
+
+
+const MyPortfolio = (props) => {
+
+  return (
+    <div>
+
+    </div>
+
+  );
+
+}
+
+export default MyPortfolio;
