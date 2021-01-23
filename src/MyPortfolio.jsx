a19510883138df03ed5d0f67bbebc29f93361261
klylylydeee <klylylydeee@gmail.com>
2021-01-23 20:18:35 +0800

    Add skills container

--- src/MyPortfolio.jsx
+++ src/MyPortfolio.jsx
@@ -67,7 +67,7 @@ const MyPortfolio = () => {
           </div>
           <div className="nav-logo">
             <p className="nav-logo-name">Klyde Guevarra</p>
-            <p>Web Developer & UI/UX Designer</p>
+            <p>Web Developer & UI Designer</p>
           </div>
         </nav>
         {/* 2 */}
@@ -78,7 +78,7 @@ const MyPortfolio = () => {
             <p className="sub-hero-text">Let me first explain what I do</p>
             <div className="hero-text-content">
               <p className="hero-text-content-first"><span>What is a <span>Web Developer </span> ?</span><br />Web developers are found working in various types of organizations, including large corporations and governments, small and medium-sized companies, or alone as freelancers. Some web developers work for one organization as a permanent full-time employee, while others may work as independent consultants, or as contractors for an agency or at home personal use. Web developers typically handle both server-side and front-end logic.</p>
-              <p className="hero-text-content-second"><span>On the other hand, what is a <span>UI/UX Designer </span> ?</span><br />UI/UX designers are responsible for overall user satisfaction with a product. Their priority is to continually look for ways to improve the product experience, even for bestselling products that have been on the market for years. They may do this by making the product faster, easier to use, or more fun and at its core encompasses the entire user experience. A good design is a good experience!</p>
+              <p className="hero-text-content-second"><span>On the other hand, what is a <span>UI Designer </span> ?</span><br />UI designers are responsible for overall user satisfaction with a product. Their priority is to continually look for ways to improve the product experience, even for bestselling products that have been on the market for years. They may do this by making the product faster, easier to use, or more fun and at its core encompasses the entire user experience. A good design is a good experience!</p>
             </div>
           </div>
           {/* Source: https://www.youtube.com/watch?v=i4Dwdo1Q1ds */}
@@ -107,7 +107,7 @@ const MyPortfolio = () => {
                   Hi, I am <span>Klyde Guevarra</span>.
                 </p>
                 <p>
-                  An aspiring <span>Web Developer</span> and <span>UI/UX Designer</span> based in Silang, Cavite.
+                  An aspiring <span>Web Developer</span> and <span>UI Designer</span> based in Silang, Cavite.
               </p>
                 <p>
                   As of now, I am still an undergraduate third year student enrolled in the degree of
@@ -198,7 +198,47 @@ const MyPortfolio = () => {
                 <span>Skills</span>
               </div>
             </div>
-            <div className="skills-content">
+            {/* 3.2.2 */}
+            <div className="different-skills-container">
+              <ul>Programming Languages:
+                <li><img src="https://i.ibb.co/dPFf0qn/58480979cef1014c0b5e4901.png" alt="a"/>Java</li>
+                <li><img src="https://i.ibb.co/Vp2d9v0/s-Ue4d-G-L-400x400.jpg" alt="s"/>Javascript</li>
+                <li><img src="https://i.ibb.co/LNvztYF/ST6u-COwkmqg-YV4-IM399y-Jmi-Ne-Lm-Qd-ICbo-Evl3-SRh-GPQDTr2-Xo-Sum4-Klip-JIi1k-Rj-IHLQ8-Xyce-FQh-JMHg.png" alt="S"/>Python</li>
+              </ul>
+              <ul>Stylesheets:
+                <li><img src="https://i.ibb.co/whhd0Vb/073-css-3.png" alt="alt"/> Sass</li>
+                <li><img src="https://cdn.worldvectorlogo.com/logos/bootstrap-5-1.svg" alt="hey"/>Bootstrap</li>
+              </ul>
+              <ul>Javascript Library/Frameworks:
+                <li><img src="https://i.ibb.co/mNZ4PZH/angular.png" alt="alt"/> Angular</li>
+                <li><img src="https://i.ibb.co/Bs8q24z/1280px-React-icon-svg.png" alt="1"/>ReactJS</li>
+                <li><img src="https://i.ibb.co/tHvNzTV/npmlogo.png" alt="npmlogo" />Node Package Manager</li>
+              </ul> 
+              <ul>Backend Technologies:
+                <li><img src="https://i.ibb.co/8886Jyg/node-Copy.png" alt="alt"/> NodeJS</li>
+                <li><img src="https://i.ibb.co/3FCKxb7/136-1363736-express-js-icon-png-transparent-png.png" alt="136"/>ExpressJS</li>
+              </ul>
+              <ul>Version Control:
+                <li><img src="https://i.ibb.co/QPSVYk6/Git-Icon-1788-C.png" alt="Git-Icon-1788-C"/>Git</li>
+              </ul>
+              <ul>Git Repositories:
+                <li><img src="https://i.ibb.co/RzWPt12/039-github-2.png" alt="039-github-2"/>Github</li>
+                <li><img src="https://i.ibb.co/c3f04ps/476-4763953-mobile-cloud-and-heroku-100-pure-java-heroku.png" alt="476"/>Heroku</li>
+              </ul>
+              <ul>Database:
+                <li><img src="https://i.ibb.co/MPybPvC/mysql.png" alt="alt"/> Mysql</li>
+                <li><img src="https://i.ibb.co/xjYhnz4/mongodb.png" alt="alt"/> MongoDB</li>
+              </ul>
+              <ul>Prototyping:
+                <li><img src="https://i.ibb.co/fr1mKX0/220px-Figma-logo-svg.png" alt="alt" />Figma</li>
+                <li><img src="https://svgshare.com/i/TQB.svg" alt="alt" />Adobe Photoshop</li>
+                <li><img src="https://i.ibb.co/ZfyHnXm/1051px-Adobe-XD-CC-icon-svg.png" alt="alt"/>Adobe XD</li>
+              </ul>
+              <ul>Routing Protocol Configurations:
+                <li><img src="https://i.ibb.co/6mZWnBN/Network-Protocol.png" alt="Network-Protocol"/>RIPv2</li>
+                <li><img src="https://i.ibb.co/6mZWnBN/Network-Protocol.png" alt="Network-Protocol"/>OSPF</li>
+                <li><img src="https://i.ibb.co/6mZWnBN/Network-Protocol.png" alt="Network-Protocol"/>EIGRP</li>
+              </ul>
             </div>
             {/* 3.2.3*/}
             <div className="services-container">
