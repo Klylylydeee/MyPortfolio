e174b9fb730eb8f9d9ac8c4ec41d2f44f8166041
klylylydeee <klylylydeee@gmail.com>
2021-01-24 12:47:07 +0800

    Projects page completed and Fix folder structure

--- src/MyPortfolio.jsx
+++ src/MyPortfolio.jsx
@@ -53,7 +53,7 @@ const MyPortfolio = () => {
 
   return (
     <Scrollbars universal thumbMinSize={100} style={{ minHeight: '100vh' }}>
-    <ReactTooltip></ReactTooltip>
+      <ReactTooltip></ReactTooltip>
       <main>
         {/* 1 */}
         <nav>
@@ -116,7 +116,7 @@ const MyPortfolio = () => {
                   as well as building things that would make a difference for you and me.
               </p>
                 <p>
-                  I am an optimistic, adaptable, and reliable person that is motivated with a goal-oriented mindset with 
+                  I am an optimistic, adaptable, and reliable person that is motivated with a goal-oriented mindset with
                   the tasks given to me. Currently, I have no work experience for neither an individual or a company but
                   given the chance I hope you'll be delighted to work with me.
               </p>
@@ -140,8 +140,8 @@ const MyPortfolio = () => {
                     <li>Private Company</li>
                     <li>May 2020 - November 2020</li>
                     <li>I am currently a student studying under the program of Bachelor of Science in Information
-                      Technology in Adventist University of the Philippines. I am currently a student studying under
-                      the program of Bachelor of Science in Information Technology in Adventist University of the
+                    Technology in Adventist University of the Philippines. I am currently a student studying under
+                    the program of Bachelor of Science in Information Technology in Adventist University of the
                       Philippines.</li>
                   </ul>
                 </div>
@@ -154,8 +154,8 @@ const MyPortfolio = () => {
                     <li>Adventist University of the Philippines</li>
                     <li>2018 - Current</li>
                     <li>I am currently a student studying under the program of Bachelor of Science in Information
-                      Technology in Adventist University of the Philippines. I am currently a student studying under
-                      the program of Bachelor of Science in Information Technology in Adventist University of the
+                    Technology in Adventist University of the Philippines. I am currently a student studying under
+                    the program of Bachelor of Science in Information Technology in Adventist University of the
                       Philippines.</li>
                   </ul>
                   <ul>
@@ -163,8 +163,8 @@ const MyPortfolio = () => {
                     <li>Adventist University of the Philippines</li>
                     <li>2016 - 2018</li>
                     <li>I am currently a student studying under the program of Bachelor of Science in Information
-                      Technology in Adventist University of the Philippines. I am currently a student studying under
-                      the program of Bachelor of Science in Information Technology in Adventist University of the
+                    Technology in Adventist University of the Philippines. I am currently a student studying under
+                    the program of Bachelor of Science in Information Technology in Adventist University of the
                       Philippines.</li>
                   </ul>
                   <ul>
@@ -172,8 +172,8 @@ const MyPortfolio = () => {
                     <li>Adventist University of the Philippines</li>
                     <li>2012 - 2016</li>
                     <li>I am currently a student studying under the program of Bachelor of Science in Information
-                      Technology in Adventist University of the Philippines. I am currently a student studying under
-                      the program of Bachelor of Science in Information Technology in Adventist University of the
+                    Technology in Adventist University of the Philippines. I am currently a student studying under
+                    the program of Bachelor of Science in Information Technology in Adventist University of the
                       Philippines.</li>
                   </ul>
                   <ul>
@@ -181,8 +181,8 @@ const MyPortfolio = () => {
                     <li>Adventist University of the Philippines</li>
                     <li>2006 - 2012</li>
                     <li>I am currently a student studying under the program of Bachelor of Science in Information
-                      Technology in Adventist University of the Philippines. I am currently a student studying under
-                      the program of Bachelor of Science in Information Technology in Adventist University of the
+                    Technology in Adventist University of the Philippines. I am currently a student studying under
+                    the program of Bachelor of Science in Information Technology in Adventist University of the
                       Philippines.</li>
                   </ul>
                 </div>
@@ -201,43 +201,44 @@ const MyPortfolio = () => {
             {/* 3.2.2 */}
             <div className="different-skills-container">
               <ul>Programming Languages:
-                <li><img src="https://i.ibb.co/dPFf0qn/58480979cef1014c0b5e4901.png" alt="a"/>Java</li>
-                <li><img src="https://i.ibb.co/Vp2d9v0/s-Ue4d-G-L-400x400.jpg" alt="s"/>Javascript</li>
-                <li><img src="https://i.ibb.co/LNvztYF/ST6u-COwkmqg-YV4-IM399y-Jmi-Ne-Lm-Qd-ICbo-Evl3-SRh-GPQDTr2-Xo-Sum4-Klip-JIi1k-Rj-IHLQ8-Xyce-FQh-JMHg.png" alt="S"/>Python</li>
+                <li><img src="https://i.ibb.co/dPFf0qn/58480979cef1014c0b5e4901.png" alt="a" />Java</li>
+                <li><img src="https://i.ibb.co/Vp2d9v0/s-Ue4d-G-L-400x400.jpg" alt="s" />Javascript</li>
+                <li><img src="https://i.ibb.co/LNvztYF/ST6u-COwkmqg-YV4-IM399y-Jmi-Ne-Lm-Qd-ICbo-Evl3-SRh-GPQDTr2-Xo-Sum4-Klip-JIi1k-Rj-IHLQ8-Xyce-FQh-JMHg.png" alt="S" />Python</li>
               </ul>
-              <ul>Stylesheets:
-                <li><img src="https://i.ibb.co/whhd0Vb/073-css-3.png" alt="alt"/> Sass</li>
-                <li><img src="https://cdn.worldvectorlogo.com/logos/bootstrap-5-1.svg" alt="hey"/>Bootstrap</li>
+              <ul className="hide-skills">Stylesheets:
+                <li><img src="https://i.ibb.co/whhd0Vb/073-css-3.png" alt="alt" /> Sass</li>
+                <li><img src="https://cdn.worldvectorlogo.com/logos/bootstrap-5-1.svg" alt="hey" />Bootstrap</li>
               </ul>
               <ul>Javascript Library/Frameworks:
-                <li><img src="https://i.ibb.co/mNZ4PZH/angular.png" alt="alt"/> Angular</li>
-                <li><img src="https://i.ibb.co/Bs8q24z/1280px-React-icon-svg.png" alt="1"/>ReactJS</li>
-                <li><img src="https://i.ibb.co/tHvNzTV/npmlogo.png" alt="npmlogo" />Node Package Manager</li>
-              </ul> 
+                <li><img src="https://i.ibb.co/mNZ4PZH/angular.png" alt="alt" /> Angular</li>
+                <li><img src="https://i.ibb.co/Bs8q24z/1280px-React-icon-svg.png" alt="1" />ReactJS</li>
+                <li className="hide-skills"><img src="https://i.ibb.co/tHvNzTV/npmlogo.png" alt="npmlogo" />Node Package Manager</li>
+              </ul>
               <ul>Backend Technologies:
-                <li><img src="https://i.ibb.co/8886Jyg/node-Copy.png" alt="alt"/> NodeJS</li>
-                <li><img src="https://i.ibb.co/3FCKxb7/136-1363736-express-js-icon-png-transparent-png.png" alt="136"/>ExpressJS</li>
+                <li><img src="https://i.ibb.co/8886Jyg/node-Copy.png" alt="alt" /> NodeJS</li>
+                <li><img src="https://i.ibb.co/3FCKxb7/136-1363736-express-js-icon-png-transparent-png.png" alt="136" />ExpressJS</li>
               </ul>
               <ul>Version Control:
-                <li><img src="https://i.ibb.co/QPSVYk6/Git-Icon-1788-C.png" alt="Git-Icon-1788-C"/>Git</li>
+                <li><img src="https://i.ibb.co/QPSVYk6/Git-Icon-1788-C.png" alt="Git-Icon-1788-C" />Git</li>
               </ul>
-              <ul>Git Repositories:
-                <li><img src="https://i.ibb.co/RzWPt12/039-github-2.png" alt="039-github-2"/>Github</li>
-                <li><img src="https://i.ibb.co/c3f04ps/476-4763953-mobile-cloud-and-heroku-100-pure-java-heroku.png" alt="476"/>Heroku</li>
+              <ul className="hide-skills">Git Repositories:
+                <li><img src="https://i.ibb.co/RzWPt12/039-github-2.png" alt="039-github-2" />Github</li>
+                <li><img src="https://i.ibb.co/c3f04ps/476-4763953-mobile-cloud-and-heroku-100-pure-java-heroku.png" alt="476" />Heroku</li>
+                <li><img src="https://i.ibb.co/7QYGN7v/logomark.png" alt="476" />Netlify</li>
               </ul>
               <ul>Database:
-                <li><img src="https://i.ibb.co/MPybPvC/mysql.png" alt="alt"/> Mysql</li>
-                <li><img src="https://i.ibb.co/xjYhnz4/mongodb.png" alt="alt"/> MongoDB</li>
+                <li><img src="https://i.ibb.co/MPybPvC/mysql.png" alt="alt" /> Mysql</li>
+                <li><img src="https://i.ibb.co/xjYhnz4/mongodb.png" alt="alt" /> MongoDB</li>
               </ul>
-              <ul>Prototyping:
+              <ul className="hide-skills">Prototyping/Wireframe:
                 <li><img src="https://i.ibb.co/fr1mKX0/220px-Figma-logo-svg.png" alt="alt" />Figma</li>
                 <li><img src="https://svgshare.com/i/TQB.svg" alt="alt" />Adobe Photoshop</li>
-                <li><img src="https://i.ibb.co/ZfyHnXm/1051px-Adobe-XD-CC-icon-svg.png" alt="alt"/>Adobe XD</li>
+                <li><img src="https://i.ibb.co/ZfyHnXm/1051px-Adobe-XD-CC-icon-svg.png" alt="alt" />Adobe XD</li>
               </ul>
-              <ul>Routing Protocol Configurations:
-                <li><img src="https://i.ibb.co/6mZWnBN/Network-Protocol.png" alt="Network-Protocol"/>RIPv2</li>
-                <li><img src="https://i.ibb.co/6mZWnBN/Network-Protocol.png" alt="Network-Protocol"/>OSPF</li>
-                <li><img src="https://i.ibb.co/6mZWnBN/Network-Protocol.png" alt="Network-Protocol"/>EIGRP</li>
+              <ul className="hide-skills">Routing Protocol Configurations:
+                <li><img src="https://i.ibb.co/6mZWnBN/Network-Protocol.png" alt="Network-Protocol" />RIPv2</li>
+                <li><img src="https://i.ibb.co/6mZWnBN/Network-Protocol.png" alt="Network-Protocol" />OSPF</li>
+                <li><img src="https://i.ibb.co/6mZWnBN/Network-Protocol.png" alt="Network-Protocol" />EIGRP</li>
               </ul>
             </div>
             {/* 3.2.3*/}
@@ -245,10 +246,10 @@ const MyPortfolio = () => {
               <p className="services-header">Services</p>
               <div className="services-container-flex">
                 <div className="services-container-content">
-                  <img src="https://i.ibb.co/WWwP21Y/website-development.png" alt="alt"/>
+                  <img src="https://i.ibb.co/WWwP21Y/website-development.png" alt="alt" />
                   <p className="services-header">Web Development</p>
-                  <p>Web Prototype Designs allows developers and users alike to get grasp of their desired visuals. 
-                    Through this service, I offer a feature-filled design that will have a memorable effect to 
+                  <p>Web Prototype Designs allows developers and users alike to get grasp of their desired visuals.
+                  Through this service, I offer a feature-filled design that will have a memorable effect to
                     everyone.</p>
                   <div className="services-container-content-img">
                     <img src="https://svgshare.com/i/TNJ.svg" alt="alt" data-tip="FrontEnd" data-class="tool-tip" />
@@ -257,10 +258,10 @@ const MyPortfolio = () => {
                   </div>
                 </div>
                 <div className="services-container-content">
-                  <img src="https://i.ibb.co/5MqJ0XR/website-design.png" alt="alt"/>
+                  <img src="https://i.ibb.co/5MqJ0XR/website-design.png" alt="alt" />
                   <p className="services-header">Web Prototype Design </p>
-                  <p>Web Prototype Designs allows developers and users alike to get grasp of their desired visuals. 
-                    Through this service, I offer a feature-filled design that will have a memorable effect to 
+                  <p>Web Prototype Designs allows developers and users alike to get grasp of their desired visuals.
+                  Through this service, I offer a feature-filled design that will have a memorable effect to
                     everyone.</p>
                   <div className="services-container-content-img">
                     <img src="https://svgshare.com/i/TQB.svg" alt="alt" data-tip="Adobe Photoshop 2021" data-class="tool-tip" />
@@ -283,6 +284,104 @@ const MyPortfolio = () => {
                 <h1>Projects</h1>
                 <span>Projects</span>
               </div>
+            </div>
+
+            <div className="container no-select">
+                <div className="team-items">
+                  <div className="item">
+                    <div className="flex">
+                      <img src="https://i.ibb.co/M6b7jcJ/Untitled-1.png" alt="/assets/img/developers/default.png" />
+
+                    </div>
+                    <div className="inner">
+                      <div className="info">
+                        <h3><a href="http://roomadvisor.herokuapp.com/">RoomsAdvisors</a></h3>
+                        <h4>Lead Developer</h4>
+                        <h5>
+                          Initially a group project requirement for Advance Web Development but turned into a
+                          personal project, nevertheless it helped me to focus and learn different technologies for 
+                          both frontend and backend development. Though I consider it as a failure I plan to recreate this in
+                          the near future.
+                        </h5>
+                        <h5 className="special-h5">
+                          <div className="special-h5-divider">
+                            <img src="https://i.ibb.co/mNZ4PZH/angular.png" alt="hey" />
+                            <img src="https://i.ibb.co/whhd0Vb/073-css-3.png" alt="hey" />
+                            <img src="https://i.ibb.co/xjYhnz4/mongodb.png" alt="hey" />
+                            <img src="https://i.ibb.co/8886Jyg/node-Copy.png" alt="hey" />
+                          </div>
+                          <div className="special-h5-divider">
+                            <a href="http://roomadvisor.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                              <img src="https://i.ibb.co/c3f04ps/476-4763953-mobile-cloud-and-heroku-100-pure-java-heroku.png" alt="476" data-tip="Heroku Deployment" data-class="tool-tip" />
+                            </a>
+                            <a href="https://github.com/Klylylydeee/angular-roomadvisor" target="_blank" rel="noopener noreferrer">
+                              <img src="https://i.ibb.co/xqVgrzc/25231-github-cat-in-a-circle-icon-vector-icon-vector-eps.png" alt="/" data-tip="Github Repository" data-class="tool-tip" />
+                            </a>
+                          </div>
+                        </h5>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="item">
+                    <div className="flex">
+                      <img src="https://i.ibb.co/1rbSDZc/Untitled-1.png" alt="/assets/img/developers/default.png" />
+
+                    </div>
+                    <div className="inner">
+                      <div className="info">
+                        <h3>Message-Mo</h3>
+                        <h4>FrontEnd Developer</h4>
+                        <h5>
+                          A project for Facebook Hackathon for the messaging category initiated by our Professor. Luckily I was invited
+                          to join and contribute to the project and though I only contributed an insignificant part of the project I value the experience
+                          and advices I learned throughout the duration of the project.
+                        </h5>
+                        <h5 className="special-h5">
+                          <div className="special-h5-divider">
+                            <img src="https://i.ibb.co/Bs8q24z/1280px-React-icon-svg.png" alt="1" />
+                            <img src="https://cdn.worldvectorlogo.com/logos/bootstrap-5-1.svg" alt="hey" />
+                          </div>
+                          <div className="special-h5-divider">
+                            <a href="https://github.com/joshuiaclan/messaging-app-2020">
+                          <img src="https://i.ibb.co/xqVgrzc/25231-github-cat-in-a-circle-icon-vector-icon-vector-eps.png" alt="/" data-tip="Github Repository" data-class="tool-tip" />
+                            </a>
+                          </div>
+                        </h5>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="item">
+                    <div className="flex">
+                      <img src="https://i.ibb.co/bb96J2x/Untitled-2.png" alt="/assets/img/developers/default.png" />
+                    </div>
+                    <div className="inner">
+                      <div className="info">
+                        <h3><a href="https://anilist-project.netlify.app/">AniList Project</a></h3>
+                        <h4>Personal Project</h4>
+                        <h5>
+                          Similar to Myanimelist.net, this web application is a database of my anime collection to easily
+                          navigate, get recommendations and ask for a copy. The main purpose of this project is to relearn
+                          the fundamentals of web development and improve my knowledge and coding skills.
+                        </h5>
+                        <h5 className="special-h5">
+                          <div className="special-h5-divider">
+                            <img src="https://i.ibb.co/XZhvfNQ/051-html-5.png" alt="hey" />
+                            <img src="https://i.ibb.co/MPybPvC/mysql.png" alt="hey" />
+                            <img src="https://i.ibb.co/8886Jyg/node-Copy.png" alt="hey" />
+                          </div>
+                          <div className="special-h5-divider">
+                            <a href="https://anilist-project.netlify.app/" target="_blank" rel="noopener noreferrer">
+                              <img src="https://i.ibb.co/7QYGN7v/logomark.png" alt="476" data-tip="Netlify Deployment" data-class="tool-tip" />
+                            </a>
+                            <a href="https://github.com/Klylylydeee/anilist-project" target="_blank" rel="noopener noreferrer">
+                              <img src="https://i.ibb.co/xqVgrzc/25231-github-cat-in-a-circle-icon-vector-icon-vector-eps.png" alt="/" data-tip="Github Repository" data-class="tool-tip" />
+                            </a>
+                          </div>
+                        </h5>
+                      </div>
+                    </div>
+                  </div>
+                </div>
             </div>
           </div>
           {/* 3.4 Contact*/}
